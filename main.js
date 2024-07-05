cc2f42778a26658d7b08586d7fae6f776535e60c
Vihtori <amicitia85@gmail.com>
2024-07-05 10:00:10 +0300

    0.8.3

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.outputColorSpace = THREE.SRGBColorSpace;
 
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setClearColor(0xFFFFFF); /* background COLOR */
+renderer.setClearColor(0x6ac3d1); /* background COLOR */
 renderer.setPixelRatio(window.devicePixelRatio);
 
 renderer.shadowMap.enabled = true;
